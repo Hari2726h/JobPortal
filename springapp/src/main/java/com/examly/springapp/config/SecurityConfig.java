7940e1a6636f823e1f3803ede8bc459da5506170
Hari2726h <727823tucs102@skct.edu.in>
2025-08-21 08:02:01 +0000

    Thu Aug 21 2025 08:02:01

--- springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
@@ -23,10 +23,4 @@
 //     public PasswordEncoder passwordEncoder(){
 //         return new BCryptPasswordEncoder();
 //     }
-
-// //     // @Bean
-// //     // public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception{
-// //     //     return config.getAuthenticationManager();
-// //     // }
-// // }
 // }
